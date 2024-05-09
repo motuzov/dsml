3282507d328894094d8d0dfaf94fa7e0c9bb7b2c
motuzov <motuzov@gmail.com>
2024-05-09 23:55:21 +0400

    demo: _volumes_to_binds set the custom volume to user

--- jhxt/jhxt/dockerspawner.py
+++ jhxt/jhxt/dockerspawner.py
@@ -6,3 +6,9 @@ class DSMLDockerSpawner(dockerspawner.DockerSpawner):
         res = await dockerspawner.DockerSpawner.start(self)
         self.log.debug("DSMLDockerSpawner")
         return res
+
+    def _volumes_to_binds(self, volumes, binds, mode="rw"):
+        self.log.debug(f"DSMLDockerSpawner volume_binds ->> {self.user.name}")
+        return dockerspawner.DockerSpawner._volumes_to_binds(
+            self, volumes, binds, mode="rw"
+        )
